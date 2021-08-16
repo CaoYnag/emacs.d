ed873e24f6fa32217727d37987fb4202d7774e89
CaoYnag <CaoYangCS@outlook.com>
2021-08-16 09:17:07 +0800

    org-crypt not found. fix it later.

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -8,22 +8,22 @@
   ;; set org-timer clock sound
   ;; then use org-timer-set-timer org-timer-pause-or-continue org-timer-stop to ctrl a timer.
   (setq org-clock-sound "~/Music/sounds/DingLing.mp3")
-  
-  ;; 自动换行
-  (add-hook 'org-mode-hook (lambda () (setq truncate-lines nil)))
-  ;; valign to support table align
-  (add-hook 'org-mode-hook #'valign-mode)
 
   ;; encrypted file
   (require 'epa-file)
   (epa-file-enable)
   (setq epa-file-encrypt-to '("CaoYangCS@outlook.com"))
-  (epa-file-select-keys 'silent)
+  ;; (epa-file-select-keys 'silent)
 
   ;; org crypt
-  (require 'org-crypt)
-  (org-crypt-use-before-save-magic)
-  (setq org-tags-exclude-from-inheritance (quote ("crypt")))
+  ;; (require 'org-crypt)
+  ;; (org-crypt-use-before-save-magic)
+  ;; (setq org-tags-exclude-from-inheritance (quote ("crypt")))
   ;; TODO conf my email addr in global.
-  (org-crypt-key "CaoYangCS@outlook.com")
+  ;; (org-crypt-key "CaoYangCS@outlook.com")
+  
+  ;; 自动换行
+  (add-hook 'org-mode-hook (lambda () (setq truncate-lines nil)))
+  ;; valign to support table align
+  (add-hook 'org-mode-hook #'valign-mode)
   )
